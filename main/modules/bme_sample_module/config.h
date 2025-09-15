a24e57753651eb5a05fb66bc43d2daf47a7164ad
xl9p <besheinuybanan25@gmail.com>
2025-09-15 11:53:49 +0000

    Split BME sensor sampling functionallity in its own module

---
+++ main/modules/bme_sample_module/config.h
@@ -0,0 +1,16 @@
+#pragma once
+#ifndef BME_SAMPLE_MODULE_CONFIG_H
+#define BME_SAMPLE_MODULE_CONFIG_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define TASK_QUEUE_SEND_TIMEOUT_MS 1000U
+
+#define BME690_I2C_ADDRESS 0x76U
+
+#ifdef __cplusplus
+}
+#endif
+#endif
